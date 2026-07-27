f2ad5b644d1d97a47a7eb061f40943537ebde647
claudiafreitasgh <cmaff1983@gmail.com>
2026-07-27 17:18:51 +0100

    Modify products

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -8,6 +8,8 @@ namespace SuperShop.Data.Entities
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage ="The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
 
 
@@ -18,10 +20,10 @@ namespace SuperShop.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
 
         [Display(Name = "Is Available")]
