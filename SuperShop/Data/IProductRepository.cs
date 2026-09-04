490b95f4c37f6bc9246c7eb134d1f3e4df55cb9a
claudiafreitasgh <cmaff1983@gmail.com>
2026-09-04 22:16:32 +0100

    Add and edit images Images on API Products User on API Products

--- SuperShop/Data/IProductRepository.cs
+++ SuperShop/Data/IProductRepository.cs
@@ -1,13 +1,11 @@
 ﻿using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using SuperShop.Data.Entities;
 
 namespace SuperShop.Data
 {
     public interface IProductRepository : IGenericRepository<Product>
     {
-
+        public IQueryable GetAllWithUsers();
 
     }
 }
---
+++ SuperShop/Models/ProductViewModal.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using SuperShop.Data.Entities;
+
+
+namespace SuperShop.Models
+{
+    public class ProductViewModal : Product
+    {
+        [Display(Name ="Image")]
+        public IFormFile ImageFile { get; set; }
+
+
+    }
+}
